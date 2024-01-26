0c8e954008fac16219836315b291141ac43a2525
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2024-01-26 07:40:45 -0300

    Novo Flashcard

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+
+def novo_flashcard(request):
+    if not request.user.is_authenticated:
+        return redirect('/usuarios/logar')
+    if request.method == 'GET':
+        return render(request, 'novo_flashcard.html')
